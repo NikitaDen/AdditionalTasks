d53bed57cf99168d359b6708bf6476ff859d68da
Nikita Denisov <nikita.denisov.1991@gmail.com>
2020-03-10 02:20:29 +0300

    Task 2 - add new messages

--- src/redux/user-reducer.js
+++ src/redux/user-reducer.js
@@ -3,9 +3,12 @@ let initialState = {
     messageText: 'Hi, my name is Nikita, and i\'d like to show some presentation about me. There is some of my advantages, check it out!',
     messageTime: '23:04',
     advantages: [
-        {advantage: 'Tall', className: 'advantages__item', id: 1},
-        {advantage: 'Medium', className: 'advantages__item advantages__item--super', id: 2},
-        {advantage: 'Short', className: 'advantages__item', id: 3},
+        {advantage: 'I', className: 'advantages__item', id: 1},
+        {advantage: 'don\'t', className: 'advantages__item advantages__item--super', id: 2},
+        {advantage: 'know', className: 'advantages__item', id: 3},
+        {advantage: 'I\'m', className: 'advantages__item', id: 3},
+        {advantage: 'too', className: 'advantages__item', id: 3},
+        {advantage: 'shy', className: 'advantages__item', id: 3},
     ]
 };
 
