e40df75f9f8cffd5371d9da878a58201cbedf560
Nikita Denisov <nikita.denisov.1991@gmail.com>
2020-03-16 22:47:37 +0300

    Updates

---
+++ precache-manifest.1cf9aef88a21477f8751b808fdef8af9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739095587c6bb6d3f33d26a626f5626a",
+    "url": "/AdditionalTasks/index.html"
+  },
+  {
+    "revision": "11b86390b775be686871",
+    "url": "/AdditionalTasks/static/css/main.67dc20c9.chunk.css"
+  },
+  {
+    "revision": "8afc23bc9bfa11b4cea4",
+    "url": "/AdditionalTasks/static/js/2.d7d3d599.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/AdditionalTasks/static/js/2.d7d3d599.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11b86390b775be686871",
+    "url": "/AdditionalTasks/static/js/main.dfddfbb5.chunk.js"
+  },
+  {
+    "revision": "06986f74a441b4342832",
+    "url": "/AdditionalTasks/static/js/runtime-main.c453eef0.js"
+  },
+  {
+    "revision": "196b2cc3fbba192b0e42734445747b96",
+    "url": "/AdditionalTasks/static/media/avatar.196b2cc3.jpg"
+  }
+]);
